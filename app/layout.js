ccfbc0872fc7d87ad2a18afa264543837c6ace06
vintage254 <derricknjuguna414@gmail.com>
2025-03-06 14:47:51 +0300

    fixed small screen ui

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import Providers from '../components/Providers';
 
 export const metadata = {
   title: 'Tuji Beads',
@@ -9,7 +10,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
